cd5ff281aa60c9a7bdd91fb92d3285aef159f19f
mumemafu <109257814+mumemafu@users.noreply.github.com>
2022-08-11 15:36:43 -0300

    Update Home.jsx
    text edit.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ useEffect ( () => {
     <div className="container">
       <h2 className="title">Melhores Filmes</h2>
       <div className="movies-container">
-        {topMovies.length === 0 && <p>Carregando porra!</p>}
+        {topMovies.length === 0 && <p>Carregando...</p>}
       {topMovies.length > 0 && 
       topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
       </div>
